2c9b81bc3c99b519249bc161e843ab04ba34fe52
Drew <WEIJIANGUO0303@GMAIL.COM>
2025-04-11 16:26:13 -0400

    Compose modifier example

--- app/src/main/java/com/laioffer/spotify/MainActivity.kt
+++ app/src/main/java/com/laioffer/spotify/MainActivity.kt
@@ -5,13 +5,22 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.laioffer.spotify.ui.theme.SpotifyTheme
 
 // customized extend AppCompatActivity
@@ -58,10 +67,78 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
     }
 }
+// composable function + previous
+@Composable
+@Preview()
+fun ArtistCardBox() {
+    Box {
+        Text("Alfred Sisley")
+        Text("3 minutes ago")
+    }
+}
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+@Preview()
+fun ArtistCardColumn() {
+    Column {
+        Text("Alfred Sisley")
+        Text("3 minutes ago")
+    }
+}
+
+@Composable
+@Preview()
+fun ArtistCardRow() {
+    Row {
+        Text("Alfred Sisley")
+        Text("3 minutes ago")
+    }
+}
+
+@Preview
+@Composable
+fun PreviewArtistCardBox() {
+    SpotifyTheme {
+        Surface {
+            ArtistCardBox()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewArtistCardRow() {
+    SpotifyTheme {
+        Surface {
+            ArtistCardRow()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewArtistCardColumn() {
+    SpotifyTheme {
+        Surface {
+            ArtistCardColumn()
+        }
+    }
+}
+
+// Compose modifiers:
+@Composable
+private fun Greeting(name: String) {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .wrapContentHeight()
+        .padding(24.dp) // Compare Padding location
+        .background(Color.Red)
+//        .padding(24.dp)
+
+    ) {
+        Text(text = "Hello,")
+        Text(text = name)
+    }
 }
 
 @Preview(showBackground = true)
